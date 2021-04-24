90bf8a53a11e5a2f0922aaf4cb17377835cda666
Murad Alizadeh <JackNickson42@gmail.com>
2021-04-24 12:21:31 +0400

    config for https to http problem, csrf disabled

---
+++ src/main/java/com/travel/load/Configuration/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.travel.load.Configuration;
+
+
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements ApplicationContextAware {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
+        http.csrf().disable();
+    }
+
+
+}
