7035e146603ab5ecbc11198b99f002a6853d5c72
elvin.ziyali <elvin.ziyali@gmail.com>
2021-05-07 23:28:25 +0400

    EmailValidator change

--- src/main/java/com/travel/load/Configuration/WebSecurityConfig.java
+++ src/main/java/com/travel/load/Configuration/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.travel.load.Configuration;
 
 
-import com.travel.load.PasswordEncoder;
 import com.travel.load.Service.AppUserService;
 import lombok.AllArgsConstructor;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -24,10 +22,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel()
+        http
+                .requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
                 .requiresSecure();
-        http.cors()
+        http
+                .cors()
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
